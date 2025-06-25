bc0a04e5fb40af557df7432819623d6210012c14
suman <suman.sumantghume12@gmail.com>
2025-06-25 22:42:14 +0530

    Auth_Next

--- src/heplers/getDataFromToken.ts
+++ src/heplers/getDataFromToken.ts
@@ -1,18 +1,12 @@
 import { NextRequest } from "next/server";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
-// Define expected structure of your token payload
-interface TokenPayload extends JwtPayload {
-  id: string;
+export const getDataFromToken = (request: NextRequest)=>{
+    try {
+        const token = request.cookies.get("token")?.value || '';
+        const decodedToken:any = jwt.verify(token,process.env.JWT_TOKEN!);
+        return decodedToken.id;
+    } catch (error) {
+        
+    }
 }
-
-export const getDataFromToken = (request: NextRequest): string | null => {
-  try {
-    const token = request.cookies.get("token")?.value || '';
-    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET!) as TokenPayload;
-
-    return decodedToken.id;
-  } catch {
-    return null;
-  }
-};
