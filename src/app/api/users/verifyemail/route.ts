f288b2efeb783769ce0c6f405581ffa474acff6b
suman <suman.sumantghume12@gmail.com>
2025-06-26 09:36:34 +0530

    Auth_Next

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -1,38 +1,39 @@
-import { connect } from "@/dbConfig/dbConfig";
-import { NextResponse, NextRequest } from "next/server";
+import {connect} from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel";
+
+
 
 connect()
 
-export async function POST(request:NextRequest) {
+
+export async function POST(request: NextRequest){
 
     try {
         const reqBody = await request.json()
         const {token} = reqBody
         console.log(token);
 
-        const user = await User.findOne({
-            verifyToken: token,
-        verifyTokenExpiry: {$gt: Date.now()}
-        });
+        const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
 
-        console.log("User :",user)
-
-        if(!user){
-            return NextResponse.json({error: "Invalid token"},{status:400})
+        if (!user) {
+            return NextResponse.json({error: "Invalid token"}, {status: 400})
         }
+        console.log(user);
 
         user.isVerfied = true;
         user.verifyToken = undefined;
-        user.verifyTokenExpiry= undefined;
+        user.verifyTokenExpiry = undefined;
         await user.save();
-
+        
         return NextResponse.json({
-            message: "Email Verified",
+            message: "Email verified successfully",
             success: true
         })
-    } catch (error) {
-        return NextResponse.json({error: error},{status:500})
+
+
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 500})
     }
-    
+
 }
